99eede3963a31573577aeded6126799b7d856e18
sonalijarhad <sonalisj.7898@gmail.com>
2019-04-25 14:31:06 +0530

    my commit

---
+++ ClassB.java
@@ -0,0 +1,14 @@
+package com.B;
+
+public class ClassB {
+	 public static void main( String [] args){
+         for(int i=0;i<=3;i++){
+		  for(int j=0;j<=i;j++){
+		  System.out.print("*");
+		  }		  System.out.println();
+		  }
+		  
+}
+
+
+}
